d5299436301d7056dac8cb85760ed76810dfdbfa
RUICHEN-722 <2040096901@qq.com>
2025-09-14 21:47:20 +0800

    if else语句练习/等级判断

--- src/Level_Division.java
+++ src/Level_Division.java
@@ -7,9 +7,9 @@ public class Level_Division {
         int grad = sc.nextInt();
         if (grad >= 90){
             System.out.println("优秀");
-        } else if (grad >= 80 && grad < 90) {
+        } else if (grad >= 80 ) {
             System.out.println("良好");
-        } else if (grad >= 60 && grad <80) {
+        } else if (grad >= 60 ) {
             System.out.println("合格");
         }else {
             System.out.println("不合格");
